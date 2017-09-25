3b4ad97877760d19b7b37e01bf74006e2e3c1a74
Paul DeSimone <pjd3949@rit.edu>
2017-09-25 09:43:47 -0400

    Completed Assignment

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/index.html`);
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
+
+// pass in the http server into socketio and grab the websocket server as io
+const io = socketio(app);
+
+io.sockets.on('connection', (socket) => {
+    console.log('connected!');
+    
+    let initialMsg = {
+        message: "Connected to socket!"
+    };
+    
+    onUpdate(socket);
+    
+    socket.join('room1');
+    socket.emit('updatePara', initialMsg);
+});
+
+const onUpdate = (sock) => {
+    const socket = sock;
+    
+    socket.on('msgToServer', (data) => {
+        io.sockets.in('room1').emit('updatePara', data);
+    });
+};
+
