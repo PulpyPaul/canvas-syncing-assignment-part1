32aa4d446118b04cd9843ce1ee38897deec211e3
Paul DeSimone <pjd3949@rit.edu>
2017-09-26 12:58:14 -0400

    Fixed error

--- src/server.js
+++ src/server.js
@@ -19,24 +19,25 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 // pass in the http server into socketio and grab the websocket server as io
 const io = socketio(app);
 
-io.sockets.on('connection', (socket) => {
-    console.log('connected!');
-    
-    let initialMsg = {
-        message: "Connected to socket!"
-    };
-    
-    onUpdate(socket);
-    
-    socket.join('room1');
-    socket.emit('updatePara', initialMsg);
-});
-
 const onUpdate = (sock) => {
-    const socket = sock;
-    
-    socket.on('msgToServer', (data) => {
-        io.sockets.in('room1').emit('updatePara', data);
-    });
+  const socket = sock;
+
+  socket.on('msgToServer', (data) => {
+    io.sockets.in('room1').emit('updatePara', data);
+  });
 };
+
+
+io.sockets.on('connection', (socket) => {
+  console.log('connected!');
+
+  const initialMsg = {
+    message: 'Connected to socket!',
+  };
+
+  onUpdate(socket);
+
+  socket.join('room1');
+  socket.emit('updatePara', initialMsg);
+});
 
